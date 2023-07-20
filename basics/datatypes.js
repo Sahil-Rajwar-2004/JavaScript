d67fe52fac5f974059c1ce61264fb48776d4546e
Sahil Rajwar <sahilrajwar2004@gmail.com>
2023-07-20 18:53:09 +0000

    datatypes.js

---
+++ basics/datatypes.js
@@ -0,0 +1,10 @@
+let a = 1;
+let c = "Sahil";
+let d = true;
+let e = null;
+let f = undefined;
+let g = Infinity;
+
+console.table([typeof(a),typeof(b),typeof(c),typeof(d),typeof(e),typeof(f)]);
+
+
