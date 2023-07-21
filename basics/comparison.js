bdd7cdb7e0f5524f31ff12d5466837d87d09d18e
Sahil Rajwar <sahilrajwar2004@gmail.com>
2023-07-21 09:26:23 +0000

    comparison.js

---
+++ basics/comparison.js
@@ -0,0 +1,9 @@
+// console.log("2">1);
+// console.log("2">=1);
+// console.log("2">"1");
+// console.log("2">="1");
+
+console.table([null > 0,null < 0,null >= 0,null <= 0,null == 0,null != 0]);
+console.table([NaN > 0,NaN < 0,NaN >= 0,NaN <= 0,NaN == 0,NaN != 0]);
+console.table([undefined > 0,undefined < 0,undefined >= 0,undefined <= 0,undefined == 0,undefined != 0]);
+
