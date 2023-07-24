11e50ecac4c881c970a968ab200a45eaa6fcfc8a
Sahil Rajwar <sahilrajwar2004@gmail.com>
2023-07-24 11:11:39 +0000

    basics/switch.js

---
+++ basics/switch.js
@@ -0,0 +1,20 @@
+let x = 2;
+let y = 0;
+let operator = "/";
+
+switch(operator){
+    case "+":
+        console.log(x+y);
+        break;
+    case "-":
+        console.log(x-y);
+        break;
+    case "*":
+        console.log(x*y);
+        break;
+    case "/":
+        console.log(x/y);
+        break;
+    default:
+        console.log("Invalid operator");
+}
