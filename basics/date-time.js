ef464f7cb5d37263fc02773b0d8cfc2135f430da
Sahil Rajwar <sahilrajwar2004@gmail.com>
2023-07-24 09:48:56 +0000

    basics/date-time.js

---
+++ basics/date-time.js
@@ -0,0 +1,11 @@
+let date = new Date();
+let idate = new Date(2023,0,9);
+
+// console.log(date);
+// console.log(date.getDate());
+console.log(date.toISOString());
+console.log(date.getMonth());
+// console.log(date.toFixed());
+// console.log(date.getFullYear());
+// console.log(date.getUTCDate());
+
