31b1305a85c2123254c312f12c5284a85c27671b
Sahil Rajwar <sahilrajwar2004@gmail.com>
2023-07-24 16:09:04 +0000

    basics/maths.js

---
+++ basics/maths.js
@@ -0,0 +1,37 @@
+let int = 3.01002341;
+let float = 3.14;
+
+// console.log(int.toFixed(5));
+// console.log(7.0.toExponential()); // why it is not giving me an error? its weird
+// console.log(Math.max(1,-1,9,2,4,0));
+// console.log(Math.random(0,10)*10);
+
+// console.log(Math.LOG10E); // log e base 10
+console.log(Math.ceil(Math.LOG10E)); // 1
+console.log(Math.ceil(0.1));
+console.log(Math.ceil(0));
+console.log(Math.ceil(0.9));
+console.log(Math.floor(0.1));
+console.log(Math.floor(0));
+console.log(Math.floor(0.9));
+console.log(Math.floor(1.1));
+
+
+// console.log(Math.PI); // 3.1415926535897932
+// console.log(Math.E); // 2.7182818284590452
+
+// console.log(Math.clz32(2));
+
+// console.log(Math.exp(2));
+// console.log(Math.pow(2,3));
+// console.log(2**3);
+
+// console.log(Math.sin(Math.PI/2));
+// console.log(Math.sin(90));
+
+// console.log(Math.tan(Math.PI/2));
+
+// console.table([Math.sin(90),Math.cos(90)])
+
+// console.log(Math.sin(Math.PI/2)/Math.cos(Math.PI/2));
+
